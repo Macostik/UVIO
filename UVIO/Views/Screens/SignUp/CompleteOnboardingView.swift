4ef15f65d425189a99f2875f15622b0991800b37
Macostik <yura-granchenko@yandex.ru>
2022-06-01 21:27:39 +0300

    feat(onboarding) complete screen

---
+++ UVIO/Views/Screens/SignUp/CompleteOnboardingView.swift
@@ -0,0 +1,20 @@
+//
+//  CompleteOnboardingView.swift
+//  UVIO
+//
+//  Created by Macostik on 01.06.2022.
+//
+
+import SwiftUI
+
+struct CompleteOnboardingView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct CompleteOnboardingView_Previews: PreviewProvider {
+    static var previews: some View {
+        CompleteOnboardingView()
+    }
+}
