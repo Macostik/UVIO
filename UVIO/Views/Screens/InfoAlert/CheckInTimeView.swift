d8b74ca76d04fbc12abe69a42bce68bb7b4d6e7a
Macostik <yura-granchenko@yandex.ru>
2022-06-28 13:51:12 +0300

    feat(main): check in time view

--- UVIO/Views/Screens/InfoAlert/CheckInTimeView.swift
+++ UVIO/Views/Screens/InfoAlert/CheckInTimeView.swift
@@ -23,8 +23,120 @@ struct CheckInTimeView_Previews: PreviewProvider {
 extension CheckInTimeView {
     var contentView: some View {
         VStack {
-            Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+            Image.timerIcon
+            Text(L10n.checkInTime)
+                .foregroundColor(Color.black)
+                .font(.poppins(.bold, size: 18))
+            Text(L10n.yourInsulinIsStillHigh)
+                .foregroundColor(Color.primaryAlertColor)
+                .font(.poppins(.bold, size: 16))
+                .multilineTextAlignment(.center)
+                .padding(.horizontal, 20)
+            RoundedRectangle(cornerRadius: 24)
+                .foregroundColor(Color.white)
+                .frame(height: 149)
+                .overlay(checkInTimeOverlay)
+                .padding(.horizontal)
+            NavigationLink(destination: {
+            }, label: {
+                ZStack {
+                    HStack {
+                        Image.nextIcon
+                            .padding()
+                    }
+                }
+                .frame(maxWidth: .infinity, maxHeight: 48, alignment: .trailing)
+                .background(Color.complementaryColor)
+                .cornerRadius(12)
+                .padding(.horizontal)
+                .overlay(buttonOverlay)
+            })
+            Button {
+            } label: {
+                Text(L10n.skipForNow)
+                    .foregroundColor(Color.black)
+                    .font(.poppins(.medium, size: 14))
+            }
+            .padding(.top, 12)
+
         }
-        .background(Color.blue)
+    }
+    var buttonOverlay: some View {
+        Text(L10n.logInsulin)
+            .foregroundColor(Color.white)
+            .font(.poppins(.medium, size: 14))
+    }
+    var checkInTimeOverlay: some View {
+        VStack {
+            Spacer()
+            HStack {
+                Spacer()
+                VStack {
+                    Text("1:30 pm")
+                        .foregroundColor(Color.black)
+                        .font(.poppins(.medium, size: 10))
+                    Text("238")
+                        .foregroundColor(Color.primaryAlertColor)
+                        .font(.poppins(.bold, size: 30))
+                    Text(L10n.mgDL)
+                        .foregroundColor(Color.black)
+                        .font(.poppins(.medium, size: 14))
+                }
+                Spacer()
+                VStack {
+                    HStack {
+                        Divider()
+                            .foregroundColor(Color.grayScaleColor)
+                    }
+                    Image.blackCircleArrowIcon
+                    HStack {
+                        Divider()
+                            .foregroundColor(Color.grayScaleColor)
+                    }
+                }
+                Spacer()
+                VStack {
+                    Text("2:45 pm")
+                        .foregroundColor(Color.black)
+                        .font(.poppins(.medium, size: 10))
+                    Text("211")
+                        .foregroundColor(Color.primaryAlertColor)
+                        .font(.poppins(.bold, size: 30))
+                    Text(L10n.mgDL)
+                        .foregroundColor(Color.black)
+                        .font(.poppins(.medium, size: 14))
+                }
+                Spacer()
+            }
+            Spacer()
+            RoundedRectangle(cornerRadius: 20)
+                .strokeBorder()
+                .foregroundColor(Color.grayScaleColor)
+                .overlay(roundOverlay)
+                .frame(height: 42)
+                .padding(.bottom, 4)
+                .padding(.horizontal, 4)
+        }
+    }
+    var roundOverlay: some View {
+        HStack {
+            Text(L10n.insulinLogged)
+                .foregroundColor(Color.black)
+                .font(.poppins(.medium, size: 10))
+            Spacer()
+            Text("5 units")
+                .foregroundColor(Color.black)
+                .font(.poppins(.bold, size: 12))
+            Capsule()
+                .foregroundColor(Color.blue.opacity(0.05))
+                .frame(width: 73, height: 16)
+                .overlay(
+                    Text(L10n.rapidAction)
+                    .foregroundColor(Color.black)
+                    .font(.poppins(.medium, size: 10))
+                )
+                .aspectRatio(contentMode: .fit)
+        }
+        .padding(.horizontal)
     }
 }
--- UVIO/Views/Screens/InfoAlert/InputInsulinView.swift
+++ UVIO/Views/Screens/InfoAlert/InputInsulinView.swift
@@ -22,7 +22,7 @@ struct InputInsulinView_Previews: PreviewProvider {
 
 extension InputInsulinView {
     var contentView: some View {
-        VStack(spacing: 12) {
+        VStack {
             Image.waringIcon
             Text(L10n.highGlucoseAlert)
                 .foregroundColor(Color.primaryAlertColor)
@@ -49,6 +49,7 @@ extension InputInsulinView {
                 .cornerRadius(12)
                 .padding(.horizontal)
                 .overlay(buttonOverlay)
+                .padding(.top, 15)
             })
             Button {
             } label: {
@@ -56,7 +57,7 @@ extension InputInsulinView {
                     .foregroundColor(Color.black)
                     .font(.poppins(.medium, size: 14))
             }
-            .padding(.top)
+            .padding(.top, 12)
 
         }
     }
